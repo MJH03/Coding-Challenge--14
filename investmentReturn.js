d88b412dd1e31d37d608eef8a84c7afec17ab0cd
MJH03 <mjhandelsman@usf.edu>
2024-07-02 14:47:09 -0400

    Develop investment return calculation module

--- investmentReturn.js
+++ investmentReturn.js
@@ -1,1 +1,5 @@
 //Module for calculating the investment return for stocks or bonds
+//Step 4:
+export function calculateInvestmentReturn(Principal, Rate, Value, Time) {
+    return Principal*(1+ Rate/Value)**(Value*Time)
+}
